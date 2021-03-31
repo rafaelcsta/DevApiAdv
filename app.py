d4080c94278bf43f8d9342a38b7ef172b2cb79dd
BlackCyber21 <rafael_for.work@hotmail.com>
2021-03-31 16:47:46 -0300

    Commit GET,PUT,DELETE

--- app.py
+++ app.py
@@ -4,22 +4,29 @@ import json
 app = Flask(__name__)
 
 desenvolvedores = [
-    {'nome':'Rafael',
-     'habilidades':['Python', 'Flask']},
-    {'nome':'Costa',
-     'habilidades':['Python', 'Html']}
+    {'nome':'Rafael','habilidades':['Python', 'Flask']},
+    {'nome':'Costa','habilidades':['Python', 'Html']}
 ]
 
-@app.route('/dev/<int:id>', methods=['GET', 'PUT'])
+@app.route('/dev/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def desenvolvedor(id):
     if request.method == 'GET':
-        desenvolvedor = desenvolvedores[id]
-        print(desenvolvedor)
-        return jsonify(desenvolvedor)
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} não existe'.format(id)
+            response = {'status':'erro', 'mensagem':mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procute o administrador da API'
+            response = {'status':'erro', 'mensagem':mensagem}
+        return jsonify(response)
     elif request.method == 'PUT':
         dados = json.loads(request.data)
         desenvolvedores[id] = dados
         return jsonify(dados)
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status':'sucesso', 'mensagem':'Registro excluído'})
 
 if __name__ == '__main__':
     app.run(debug=True)
