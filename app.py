685c18fe1a95d216d8ab0081adf308d8907bd83a
BlackCyber21 <rafael_for.work@hotmail.com>
2021-03-31 03:42:37 -0300

    Commit Aula 4

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+desenvolvedores = [
+    {'nome':'Rafael',
+     'habilidades':['Python', 'Flask']},
+    {'nome':'Costa',
+     'habilidades':['Python', 'Html']}
+]
+
+@app.route('/dev/<int:id>', methods=['GET', 'PUT'])
+def desenvolvedor(id):
+    if request.method == 'GET':
+        desenvolvedor = desenvolvedores[id]
+        print(desenvolvedor)
+        return jsonify(desenvolvedor)
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
+
+if __name__ == '__main__':
+    app.run(debug=True)
